e08969f058f238980e11eea3f6a5a5ff5486fdc4
Koimtzoglou <dimitriskoimtzoglou@gmail.com>
2017-06-28 11:41:52 +0200

    Fix bugs

--- Data_grouping_and_merging.py
+++ Data_grouping_and_merging.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os 
 import numpy as np
 import configuration as conf
-
+from datetime import datetime
 
 class files_to_df(object):
     
@@ -49,20 +49,34 @@ class files_to_df(object):
         df = dict.fromkeys(self.keys)
         for key in self.keys:
             resistances = ['resistance {}'.format(i) for i in np.arange(int(key))]
-            df_names=['Date','time','minimum', 'maximum', 'wire resistance'] + resistances 
+            df_names=['Date','time','minimum', 'maximum', 'wire resistance'] + resistances  
+            types = {}
+            for name in df_names:
+                if (name == 'Date') or (name=='time') :
+                    types[name] = 'str'
+                else:
+                    types[name] = 'float'
+                
+            print (df_names)
             appended_data = []
             for file in self.all_channels[key]:
                 path =  os.path.join(self.directory, file)
-                print (path)
+                #dateparse = lambda x: pd.to_datetime(x, format='%y-%m-%d %H:%M:%S',errors='ignore')
                 array = pd.read_csv(path, 
-                                    encoding='cp1252',
+                                    #encoding='cp1252',
+                                    encoding='iso-8859-1',
                                     header = None, 
                                     sep = ';', 
                                     usecols = np.arange((len(df_names))),
                                     names= df_names,
-                                    parse_dates=[[0,1]]
+#                                    parse_dates=[['Date', 'time']],
+#                                    infer_datetime_format = True
                                     )
-
+                array['minimum'] = np.dot(array['minimum'], 100)
+                array['maximum'] =  np.dot(array['maximum'], 100)
+                array['Time_Stamp'] = array['Date'] + ' ' + array['time']
+                array['Time_Stamp'] = pd.to_datetime(array['Time_Stamp'],format='%y-%m-%d %H:%M:%S',errors='coerce')
+                array['Time_Stamp'] = array['Time_Stamp'].dt.strftime('%m/%d/%Y')
                 appended_data.append(array)
             appended_data = pd.concat(appended_data, axis=0)
             df[key] = appended_data
